7196b0c73e760859c76a469b57bc012044814afe
maybemkl <mikael.brunila@gmail.com>
2020-07-13 19:49:02 +0000

    remove and reshuffle files, comment coherence

--- coherence.py
+++ coherence.py
@@ -6,12 +6,21 @@ import pandas as pd
 import pickle
 
 def main():
-    base_path = "topic_models/sent_embedder/"
-    output_coherence(f"{base_path}features/features_sent_embeddings.pickle",
-                     f"{base_path}topics",
-                     embed_name = "sent_embedder",
-                     coherence_type = ['c_v', 'c_uci', 'c_npmi', 'u_mass'])
+    base_path = "topic_models/"
+    c_dfs = []
+    for _, dirs, _ in os.walk(base_path):
+        dirs = (d for d in dirs if (not d[0] == '.') and (not d[0].startswith('t')))
+        for d in dirs:
+            c_dfs.append(output_coherence(f"{base_path}{d}/features.pkl",
+                                  f"{base_path}{d}/topics",
+                                  embed_name = d,
+                                  coherence_type = ['c_v', 'c_uci', 'c_npmi', 'u_mass']))
+    output = pd.concat(c_dfs).groupby('ct', group_keys = False).apply(
+        pd.DataFrame.sort_values, 'coherence', ascending=False).reset_index(drop = 'True')
+    output.to_csv("coherence_scores_v3.csv", index=False)
 
+# Use Gensim's CoherenceModel() to return model coherence:
+# https://radimrehurek.com/gensim/models/coherencemodel.html
 def get_coherence(features, topics, coherence_type = 'c_v'):
     dct = Dictionary(features)
     topics = topics.values.tolist()
@@ -30,12 +39,19 @@ def get_coherence(features, topics, coherence_type = 'c_v'):
     
     return coherence
 
+# Gather coherence scores from several coherence  dataframe.
 def output_coherence(features_path, topics_dir, embed_name, coherence_type, topic_model = "kmeans"):
     topic_paths = []
     features = pickle.load(open(features_path, 'rb'))
     df = {'embeddings': [], 'model': [], 'components':[], 'topics': [], 
           'ngrams_per_topic':[],'ct': [], 'coherence': [], 'hashtags': []}
-
+    if 'no_hashtag' in embed_name:
+        ht = 'no'
+        embedding = embed_name.split('no_hashtag')[0]
+    else:
+        ht = 'yes'
+        embedding = embed_name.split('hashtag')[0]
+    
     for file in os.listdir(topics_dir):
         topic_paths.append((f"{topics_dir}/{file}", file))
 
@@ -43,17 +59,16 @@ def output_coherence(features_path, topics_dir, embed_name, coherence_type, topi
         topics = pickle.load(open(path[0], 'rb'))
         for ct in coherence_type:
             coherence = get_coherence(features, topics, coherence_type = ct)
-            df['embeddings'].append(embed_name)
+            df['embeddings'].append(embedding)
             df['model'].append(topic_model)
             df['components'].append(path[1].split('.')[0])
             df['topics'].append(10)
             df['ngrams_per_topic'].append(10)
             df['ct'].append(ct)
             df['coherence'].append(coherence)
-            df['hashtags'].append('no')
-    df = pd.DataFrame.from_dict(df).groupby('ct', group_keys = False).apply(
-        pd.DataFrame.sort_values, 'coherence', ascending=False).reset_index(drop = 'True')
-    df.to_csv("coherence_scores.csv", index=False)
+            df['hashtags'].append(ht)
+    df = pd.DataFrame.from_dict(df)
+    return df
 
 if __name__ == '__main__':
     main()
