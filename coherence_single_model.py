23e26261e6c5bcb1eb4f5ab6ee48a1238db3b745
maybemkl <mikael.brunila@gmail.com>
2020-08-03 17:06:09 +0000

    script for outputting individual coherence values

---
+++ coherence_single_model.py
@@ -0,0 +1,46 @@
+from gensim.corpora import Dictionary
+from gensim.models.coherencemodel import CoherenceModel
+from gensim.test.utils import common_corpus, common_dictionary
+
+import os
+import pandas as pd
+import pickle
+
+def main():
+    topics = [["progression", "visibility"], ["city", "window"], ["continue", "snow"]]
+    features = [['ohsttkoorj'], 
+                ['progression', 'storm', 'qdosgepicb'], 
+                ['snow', 'band', 'starting', 'pivot', 'avalon', 'heavy snow', 'continue', 'hour', 'wind', 'thqvt6xlwl'], 
+                ['anytime', 'speculated', 'yesterday', 'severity', 'storm', 'headache', 'gonna', '4wziule2dc'], 
+                ['eddiesheerr', 'eddie', 'twn', 'forecast', 'wind', 'starting', 'ease', 'hour', '00pm', 'accurate'], 
+                ['picture', 'crazyy', 'complain', 'ottawa', 'winter'], 
+                ['visibility', 'reduced', 'are', 'towngfw', 'hour', 'motorist', 'stayed', 'town', 'crew', 'bezohqsext'], 
+                ['hiphops lit dope flow', 'gotdrip bing tiktok feelin', 'therealbigthump', 'sddgjenmwx'], 
+                ['bonkers', 'snow', 'window', 'nkwgoh4ark'], 
+                ['snow drift', 'taller', 'city', 'issued', 'garbage', 'bin', 'telling', 'set', 'stair', 'fhy7aovrzc']]
+    for c in ['c_v', 'c_uci', 'c_npmi', 'u_mass']:
+        coherence = get_coherence(features, topics, coherence_type = c)
+        print(f"The coherence value of the model using the {c} metric is: {coherence}.")
+
+# Use Gensim's CoherenceModel() to return model coherence:
+# https://radimrehurek.com/gensim/models/coherencemodel.html
+def get_coherence(features, topics, coherence_type = 'c_v'):
+    dct = Dictionary(features)
+    #topics = topics.values.tolist()
+
+    if coherence_type == 'u_mass':
+        bow_corpus = [dct.doc2bow(f) for f in features]
+        cm = CoherenceModel(topics=topics, corpus=bow_corpus, dictionary=dct, coherence=coherence_type)
+        coherence = cm.get_coherence()  # get coherence value
+
+    elif coherence_type in ['c_v', 'c_uci', 'c_npmi']:
+        cm = CoherenceModel(topics=topics, texts = features, dictionary=dct, coherence=coherence_type)
+        coherence = cm.get_coherence()  # get coherence value
+
+    else:
+        print(f"'{coherence_type}' is not a coherence model. Use one of the following arguments: 'u_mass', 'c_v', 'c_uci', 'c_npmi'.")
+    
+    return coherence
+
+if __name__ == '__main__':
+    main()
