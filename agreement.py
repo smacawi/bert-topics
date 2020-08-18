8088ba2152774adffaf48cf0683a4526a1891f98
maybemkl <mikael.brunila@gmail.com>
2020-08-18 19:34:29 +0000

    measure agreement of predictions between unsupervised and supervised models

---
+++ agreement.py
@@ -0,0 +1,48 @@
+from sklearn.metrics import confusion_matrix
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+import seaborn as sns
+
+BASE_URL = "topic_models/supervised_sent_embeddings_hashtags/9/"
+LABELS_PATH = f"{BASE_URL}labels.pkl"
+PREDS_PATH = f"{BASE_URL}preds.pkl"
+
+superv = pickle.load(open(PREDS_PATH, 'rb'))
+usuperv = pickle.load(open(LABELS_PATH, 'rb'))
+usuperv = list(map(str, usuperv)) 
+
+
+#cmtx = pd.DataFrame(
+#    confusion_matrix(y_true, y_pred, labels=['yes', 'no']), 
+#    index=['true:yes', 'true:no'], 
+#    columns=['pred:yes', 'pred:no']
+#)
+# s print(cmtx)
+
+cm = confusion_matrix(superv, usuperv, normalize='all')
+
+cluster = pd.Series(usuperv)
+pred = pd.Series(superv)
+
+ct = pd.crosstab(pred, cluster, rownames=['Prediction'], colnames=['Cluster'], margins=False).apply(lambda r: round(r, 0))
+ct_norm_cluster = pd.crosstab(pred, cluster, rownames=['Prediction'], colnames=['Cluster'], margins=False, normalize = 'columns').apply(lambda r: round(r, 2))
+ct_norm_pred = pd.crosstab(pred, cluster, rownames=['Prediction'], colnames=['Cluster'], margins=False, normalize = 'index').apply(lambda r: round(r, 2))
+
+ct.to_csv("label_pred_ct.csv", index=True)
+ct_norm_cluster.to_csv("ct_norm_cluster.csv", index=True)
+ct_norm_pred.to_csv("ct_norm_pred.csv", index=True)
+
+ct_p = sns.heatmap(ct, cmap="YlGnBu", annot=True, cbar=False, fmt='d')
+ct_p.figure.savefig("ct_p.png",dpi=300, bbox_inches = "tight")
+plt.clf()
+
+ct_norm_cluster_p = sns.heatmap(ct_norm_cluster,
+            cmap="YlGnBu", annot=True, cbar=False)
+ct_norm_cluster_p.figure.savefig("ct_norm_cluster_p.png",dpi=300, bbox_inches = "tight")
+plt.clf()
+
+ct_norm_pred_p = sns.heatmap(ct_norm_pred,
+            cmap="YlGnBu", annot=True, cbar=False)
+ct_norm_pred_p.figure.savefig("ct_norm_pred_p.png",dpi=300, bbox_inches = "tight")
