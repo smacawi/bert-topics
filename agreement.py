9e58eca96a1dca37212257765aee14cf6da1a6c7
maybemkl <mikael.brunila@gmail.com>
2020-08-24 17:24:30 +0000

    add overall accuracy

--- agreement.py
+++ agreement.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from sklearn.metrics import confusion_matrix
 
 import matplotlib.pyplot as plt
@@ -13,22 +14,18 @@ superv = pickle.load(open(PREDS_PATH, 'rb'))
 usuperv = pickle.load(open(LABELS_PATH, 'rb'))
 usuperv = list(map(str, usuperv)) 
 
-
-#cmtx = pd.DataFrame(
-#    confusion_matrix(y_true, y_pred, labels=['yes', 'no']), 
-#    index=['true:yes', 'true:no'], 
-#    columns=['pred:yes', 'pred:no']
-#)
-# s print(cmtx)
-
 cm = confusion_matrix(superv, usuperv, normalize='all')
 
 cluster = pd.Series(usuperv)
 pred = pd.Series(superv)
 
 ct = pd.crosstab(pred, cluster, rownames=['Prediction'], colnames=['Cluster'], margins=False).apply(lambda r: round(r, 0))
-ct_norm_cluster = pd.crosstab(pred, cluster, rownames=['Prediction'], colnames=['Cluster'], margins=False, normalize = 'columns').apply(lambda r: round(r, 2))
-ct_norm_pred = pd.crosstab(pred, cluster, rownames=['Prediction'], colnames=['Cluster'], margins=False, normalize = 'index').apply(lambda r: round(r, 2))
+ct_norm_cluster = pd.crosstab(pred, cluster, rownames=['Prediction'], 
+                              colnames=['Cluster'], margins=False, 
+                              normalize = 'columns').apply(lambda r: round(r, 2))
+ct_norm_pred = pd.crosstab(pred, cluster, rownames=['Prediction'], 
+                           colnames=['Cluster'], margins=False, 
+                           normalize = 'index').apply(lambda r: round(r, 2))
 
 ct.to_csv("label_pred_ct.csv", index=True)
 ct_norm_cluster.to_csv("ct_norm_cluster.csv", index=True)
@@ -38,11 +35,15 @@ ct_p = sns.heatmap(ct, cmap="YlGnBu", annot=True, cbar=False, fmt='d')
 ct_p.figure.savefig("ct_p.png",dpi=300, bbox_inches = "tight")
 plt.clf()
 
-ct_norm_cluster_p = sns.heatmap(ct_norm_cluster,
-            cmap="YlGnBu", annot=True, cbar=False)
+ct_norm_cluster_p = sns.heatmap(ct_norm_cluster,cmap="YlGnBu", annot=True, cbar=False)
 ct_norm_cluster_p.figure.savefig("ct_norm_cluster_p.png",dpi=300, bbox_inches = "tight")
 plt.clf()
 
-ct_norm_pred_p = sns.heatmap(ct_norm_pred,
-            cmap="YlGnBu", annot=True, cbar=False)
+ct_norm_pred_p = sns.heatmap(ct_norm_pred,cmap="YlGnBu", annot=True, cbar=False)
 ct_norm_pred_p.figure.savefig("ct_norm_pred_p.png",dpi=300, bbox_inches = "tight")
+
+max_overlap = ct_norm_cluster.max()
+freqs = Counter(cluster)
+accuracy = sum([max_overlap[i]*(freqs[i]/len(cluster)) for i in max_overlap.index])
+
+print(f"The overall accuracy of the unsupervised classifier is: {accuracy}")
